372b268f1afd66d20df95a64d641d8d600f6acdf
Hoàng Lâm <vuhoanglam2000vn@gmail.com>
2025-02-04 11:08:30 +0700

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,7 +3,7 @@ import {getCorsWhitelist} from "../helpers/utils/utils";
 
 export const corsOptions: CorsOptions = {
     origin: function (origin, callback) {
-        if (getCorsWhitelist().indexOf(origin!) !== -1) {
+        if (getCorsWhitelist().indexOf(origin!) !== -1 || !origin) {
             callback(null, true);
         } else {
             callback(new Error("Not allowed by CORS"));
