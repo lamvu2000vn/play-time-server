8a21684c29ba30095a8f895e043df0b76a39afc3
Hoàng Lâm <vuhoanglam2000vn@gmail.com>
2025-02-07 12:43:12 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ import {corsOptions} from "./config/cors";
     app.use(express.json());
     app.use(express.urlencoded({extended: false}));
     app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, "public")));
+    app.use("/assets", express.static(path.join(__dirname, "public/assets")));
     app.use(cors(corsOptions));
 
     initializeSocket(httpServer);
