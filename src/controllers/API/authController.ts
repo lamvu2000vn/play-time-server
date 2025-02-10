ef4a15b694fa390e4243f970decd7ac3374bc1e8
Hoàng Lâm <vuhoanglam2000vn@gmail.com>
2025-02-10 16:17:24 +0700

    Update .gitignore

--- src/controllers/API/authController.ts
+++ src/controllers/API/authController.ts
@@ -86,13 +86,6 @@ export const login = async (req: Request, res: Response) => {
         await newToken.save();
 
         if (refreshToken) {
-            // res.cookie("refreshToken", refreshToken, {
-            //     httpOnly: true,
-            //     secure: process.env.NODE_ENV === "production",
-            //     sameSite: process.env.NODE_ENV === "production" ? "none" : undefined,
-            //     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-            // });
-
             const isProduction = process.env.NODE_ENV === "production";
 
             res.setHeader(
